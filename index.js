0d412f8aee3efb3a381d5136fbda871c9096f347
Mendy <mendybeltcohen@gmail.com>
2018-04-24 14:23:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 var katzDeli = [];
 
-/* function takeANumber(currentLine, name) {
-   currentLine.push(name);
- currentLine.length;
-return currentLine;
-} */
-
-
-function takeANumber(currentLine, name) {
+  function takeANumber(currentLine, name) {
    currentLine.push(name);
 return `Welcome, ${name}. You are number ${currentLine.length} in line.`;
 }

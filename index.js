ecb891912e6f524ff74e279db5fe132d389c1efc
Mendy <mendybeltcohen@gmail.com>
2018-04-24 16:31:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,38 +5,9 @@ var katzDeli = [];
 return `Welcome, ${name}. You are number ${currentLine.length} in line.`;
 }
 
-/* function nowServing(firstOnLine) {
-if(firstOnLine === 1) {
-  return firstOnLine;
-} else {return "There is nobody waiting to be served!"}
-} */ 
-
-/* function nowServing(firstOnLine) {
-  firstOnLine.shift();
-if(firstOnLine.length === 0) {
-  return "There is nobody waiting to be served!";
-} 
-} 
-
-/* function nowServing(line) {
-  if (line.length === 0) {
-    return "There is nobody waiting to be served!";
-  } else {
-    var person = line.shift();
-    return `Currently serving ${shift[0]}.`;
-  }
-} */
-
-/* function nowServing(firstOnLine) {
-if(firstOnLine.length === 0) {
-  return "There is nobody waiting to be served!";
-} else {return firstOnLine.shift()}
-} */
-
 function nowServing(shift) {
  var please = `Currently serving ${shift[0]}.`;
  shift.shift();
- //return please;
  if(shift.length === 0) {
    return "There is nobody waiting to be served!";
  } return please;

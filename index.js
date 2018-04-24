b6004a49b55365e0327606aa0c4eb856b806511e
Mendy <mendybeltcohen@gmail.com>
2018-04-24 13:15:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 
 function takeANumber(a, b) {
  var katzDeliLine = [];
- katzDeliLine.push(takeANumber(katzDeliLine, "Ada"));
+  takeANumber(katzDeliLine, "Ada");
   return katzDeliLine;
 }
 

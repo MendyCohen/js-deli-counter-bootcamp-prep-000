019d6e1f6ec3dd31d9c727c9a31c97cae89d96a2
Mendy <mendybeltcohen@gmail.com>
2018-04-24 16:48:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function nowServing(shift) {
 function currentLine(line) {
   if(line.length === 0) {
      return 'The line is currently empty.';
-  } else {return `The line is currently:${line.length}`}
+  } else {return `The line is currently:${line}`}
 }
 

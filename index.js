aa835cd2646a4a217e7af0ec183df7e6fc717f0d
Mendy <mendybeltcohen@gmail.com>
2018-04-24 15:18:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ return `Welcome, ${name}. You are number ${currentLine.length} in line.`;
 }
 
 function nowServing(firstOnLine) {
-   do{"There is nobody waiting to be served!"}
-   while()
+   do{"There is nobody waiting to be served!"
+     
+   }  while(firstOnLine.)
 }

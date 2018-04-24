fb4e746b3c7c6d8093bf4bf7ad908ba03b0e83a6
Mendy <mendybeltcohen@gmail.com>
2018-04-24 15:08:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ return `Welcome, ${name}. You are number ${currentLine.length} in line.`;
 }
 
 function nowServing(firstOnLine) {
+  return firstOnLine;
   if(firstOnLine === 0) {
     "There is nobody waiting to be served!";
   } else {

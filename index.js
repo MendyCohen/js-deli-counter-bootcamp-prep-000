2970b407e4dbc11ca47168e5b6ff69571b1135ec
Mendy <mendybeltcohen@gmail.com>
2018-04-24 16:15:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ if(firstOnLine.length === 0) {
 }
 
 function nowServing(shift) {
- shift.shift();  return `Currently serving ${shift[0]}`;
-// return `Currently serving ${shift[0]}`;
+ 
+ var line = `Currently serving ${shift[0]}`;
+ shift.shift();
+ return line;
 }
 

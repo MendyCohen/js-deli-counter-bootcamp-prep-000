d2907f867ce909f873fb87ccba98c2add0660c81
Mendy <mendybeltcohen@gmail.com>
2018-04-24 15:37:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,17 @@ if(firstOnLine.length === 0) {
 } 
 } */
 
-function nowServing(line) {
+/* function nowServing(line) {
   if (line.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
     var person = line.shift();
     return "now serving " + person;
   }
+} */
+
+function nowServing(firstOnLine) {
+if(firstOnLine === 0) {
+  return firstOnLine;
+} else {return "There is nobody waiting to be served!"}
 }
